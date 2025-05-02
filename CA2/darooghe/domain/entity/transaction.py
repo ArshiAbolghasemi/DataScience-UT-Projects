b874a4920270c55901bc5b227c65748ead688461
ArshiAbolghasemi <arshiabolghasemi@gmail.com>
2025-05-02 20:56:47 +0330

    feat(ca2): adding validate method to transaction

--- CA2/darooghe/domain/entity/transaction.py
+++ CA2/darooghe/domain/entity/transaction.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass
-from datetime import datetime
-from typing import Optional
+from datetime import UTC, datetime, timedelta
+from typing import Dict, Optional, Tuple
 from enum import Enum
 
-from darooghe.domain.entity.device import Device
+from darooghe.domain.entity.device import Device, OS
+from darooghe.domain.entity.error import ErrorCode
 from darooghe.domain.entity.location import Location
 
 from darooghe.domain.util.serialization import serializable
@@ -75,3 +76,54 @@ class Transaction:
     customer_type: CustomerType
     risk_level: RiskLevel
     failure_reason: Optional[FailureReason]
+
+    def validate(self) -> Dict[ErrorCode, str]:
+        errors = {}
+
+        amount_consistent, amount_msg = self.__has_amount_consistency()
+        if not amount_consistent:
+            errors[ErrorCode.AMOUNT_CONSISTENCY] = amount_msg
+
+        time_valid, time_msg = self.__has_time_warping()
+        if not time_valid:
+            errors[ErrorCode.TIME_WARPING] = time_msg
+
+        device_valid, device_msg = self.__has_device_mismatch()
+        if not device_valid:
+            errors[ErrorCode.DEVICE_MISMATCH] = device_msg
+
+        return errors
+
+    def __has_amount_consistency(self) -> Tuple[bool, Optional[str]]:
+        if self.total_amount == self.amount + self.vat_amount + self.commission_amount:
+            return True, None
+
+        return (
+            False,
+            "total amount does not match sum amount, vat_amount and commission_amount",
+        )
+
+    def __has_time_warping(self) -> Tuple[bool, Optional[str]]:
+        current_time = datetime.now(UTC)
+        transaction_time = self.timestamp
+        if transaction_time > current_time:
+            return (
+                False,
+                f"Future timestamp: {transaction_time}, current_time: {current_time}",
+            )
+        elif transaction_time < (current_time - timedelta(days=1)):
+            return False, f"Timestamp too old: {transaction_time}"
+
+        return True, None
+
+    def __has_device_mismatch(self) -> Tuple[bool, Optional[str]]:
+        if self.payment_method != PaymentMethod.MOBILE:
+            return True, None
+
+        if self.device_info is None:
+            return True, None
+
+        if self.device_info.os in [OS.IOS, OS.ANDROID]:
+            return True, None
+
+        return False, f"Invalid OS '{self.device_info.os.value}' for mobile payment"
