d6fab8711c5ba8ba019bf88597715cb898766baa
ArshiAbolghasemi <arshiabolghasemi@gmail.com>
2025-05-02 20:55:43 +0330

    adding new kafka topic and group id

--- CA2/darooghe/infrastructure/service/messaging/kafka_config.py
+++ CA2/darooghe/infrastructure/service/messaging/kafka_config.py
@@ -1,2 +1,10 @@
 class KafkaTopics:
     DAROOGHE_TRANSACTIONS = "darooghe.transactions"
+    DAROOGHE_ERROR_LOGS = "darooghe.error_logs"
+
+
+class KafkaGroups:
+    DAROOGHE_TRANSACTIONS_CONSUMER = "darooghe-transactions-consumer"
+
+
+LOCAL_KAFKA_BROKER = "localhost:9092"
