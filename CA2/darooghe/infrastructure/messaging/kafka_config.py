cf552d8beab02cc2d6129f9837877f050ce425a3
ArshiAbolghasemi <arshiabolghasemi@gmail.com>
2025-05-05 17:44:15 +0330

    fix(ca2): fix variable name

--- CA2/darooghe/infrastructure/messaging/kafka_config.py
+++ CA2/darooghe/infrastructure/messaging/kafka_config.py
@@ -4,7 +4,7 @@ import os
 class Kafka:
 
     class Config:
-        KAFKA_BROKER = os.getenv("KAFKA_BROKER", "localhost:9092")
+        KAFKA_BROKER = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
 
     class Topics:
         DAROOGHE_TRANSACTIONS = "darooghe.transactions"
