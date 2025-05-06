a3e3c3c5505693eb11b92bfbdf3e825af2697253
ArshiAbolghasemi <arshiabolghasemi@gmail.com>
2025-05-07 01:47:03 +0330

    feat(ca2): adding TimeOfDay util

---
+++ CA2/darooghe/domain/util/time.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+
+class TimeOfDay(Enum):
+    MORNING = (6, 11, "morning")
+    AFTERNOON = (12, 16, "afternoon")
+    EVENING = (17, 20, "evening")
+    NIGHT = (21, 5, "night")
+
+    @property
+    def start(self):
+        return self.value[0]
+
+    @property
+    def end(self):
+        return self.value[1]
+
+    @property
+    def label(self):
+        return self.value[2]
