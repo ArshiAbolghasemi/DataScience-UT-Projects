57f81f506b7651ff6c0a1003e5f990f4603b9c81
ArshiAbolghasemi <arshiabolghasemi@gmail.com>
2025-05-08 11:55:14 +0330

    chore(ca2): changin spark session manager

--- CA2/darooghe/infrastructure/data_processing/spark_config.py
+++ CA2/darooghe/infrastructure/data_processing/spark_config.py
@@ -8,7 +8,16 @@ class Spark:
         COMMISSION_ANALYSIS = "commission_analysis"
 
     class Config:
-        JARS_PACKAGES = os.getenv("SPARK_JARS_PACKAGES")
+        MONGO_JARS_PACKAGES = os.getenv("SPARK_MONGO_JARS_PACKAGES")
+        KAFKA_JARS_PACKAGES = os.getenv("SPARK_KAFKA_JARS_PACKAGES")
         SPARK_MASTER = os.getenv("SPARK_MASTER")
         DRIVER_HOST = os.getenv("SPARK_DRIVER_HOST")
         DRIVER_BIND_ADDRESS = os.getenv("SPARK_DRIVER_BIND_ADDRESS")
+
+        STREAMING_KAFKA_MAX_RATE_PER_PARTITION = os.getenv(
+            "STREAMING_KAFKA_MAX_RATE_PER_PARTITION", 100
+        )
+        SPARK_SQL_SUFFLE_PARTITION = os.getenv("SPARK_SQL_SUFFLE_PARTITION", 4)
+
+        class CheckPoints:
+            FRAUD_DETECTION_DIR = ""
--- CA2/darooghe/infrastructure/data_processing/spark_session_manager.py
+++ CA2/darooghe/infrastructure/data_processing/spark_session_manager.py
@@ -13,6 +13,20 @@ def create_mongo_session(app_name: str) -> SparkSession:
         .config("spark.mongodb.write.connection.uri", Mongo.Config.MONGO_URI)
         .config("spark.driver.host", Spark.Config.DRIVER_HOST)
         .config("spark.driver.bindAddress", Spark.Config.DRIVER_BIND_ADDRESS)
-        .config("spark.jars.packages", Spark.Config.JARS_PACKAGES)
+        .config("spark.jars.packages", Spark.Config.MONGO_JARS_PACKAGES)
+        .getOrCreate()
+    )
+
+
+def create_kafka_session(app_name: str) -> SparkSession:
+    return (
+        SparkSession.Builder()
+        .appName(app_name)
+        .config(
+            "spark.streaming.kafka.maxRatePerPartition",
+            Spark.Config.STREAMING_KAFKA_MAX_RATE_PER_PARTITION,
+        )
+        .config("spark.sql.shuffle.partitions", Spark.Config.SPARK_SQL_SUFFLE_PARTITION)
+        .config("spark.jars.packages", Spark.Config.KAFKA_JARS_PACKAGES)
         .getOrCreate()
     )
