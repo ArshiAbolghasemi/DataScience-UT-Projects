cfe8ce132a0e35522a1688415d8c81d4c7bdf4dd
ArshiAbolghasemi <arshiabolghasemi@gmail.com>
2025-05-09 21:15:25 +0330

    feat(ca2): adding topics for commission analytics

--- CA2/darooghe/infrastructure/messaging/kafka_config.py
+++ CA2/darooghe/infrastructure/messaging/kafka_config.py
@@ -10,6 +10,9 @@ class Kafka:
         DAROOGHE_TRANSACTIONS = "darooghe.transactions"
         DAROOGHE_ERROR_LOGS = "darooghe.error_logs"
         DAROOGHE_FRAUD_ALERTS = "darooghe.fraud_alerts"
+        DAROOGHE_COMMISSION_BY_TYPE = "darooghe.commission_by_type"
+        DAROOGHE_COMMISSION_RATIO_BY_MERCHANT_CATEGORY = "darooghe.commission_ratio_per_merchant_category"
+        DAROOGHE_TOP_MERHCANT_COMMISSION = "darooghe.top_merchant_commission"
 
     class Groups:
         DAROOGHE_TRANSACTIONS_CONSUMER = "darooghe-transactions-consumer"
